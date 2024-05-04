bf37dc4ebb89c01560765d534b590c2b45f960da
fishyy119 <1830979240@qq.com>
2024-05-04 15:59:02 +0800

    orbital_solving_algorithms: 闭合拉格朗日系数（不递推，有问题

--- orbital_solving_algorithms.py
+++ orbital_solving_algorithms.py
@@ -3,13 +3,13 @@
     数值积分方法求解轨道
     开普勒方法求解轨道
     拉格朗日系数法求解轨道
-fishyy  24.04.13 -- 24.04.
+fishyy  24.04.13 -- 24.05.
 """
 import numpy as np
 from functools import partial
 from numerical_methods import newton_method, runge_kutta45
-from orbital_conversion import kepler_to_state, state_to_kepler, calculate_M
-from two_body_orbit_dynamics import keplers_equation_and_derivative, two_body_dynamics, lagrange_coefficient
+from orbital_conversion import kepler_to_state, state_to_kepler, calculate_M, E_to_f
+from two_body_orbit_dynamics import keplers_equation_and_derivative, two_body_dynamics, lagrange_coefficient, lagrange_coefficient_2
 
 def solve_orbit_integrate(mu, r_0, t_span, **kwargs):
     """
@@ -102,7 +102,7 @@ def solve_orbit_kepler(mu, r_0, t_target, **kwargs):
 
 def solve_orbit_lagrange(mu, r_0, t_target):
     """
-    拉格朗日系数法求解轨道。
+    拉格朗日系数法(级数)求解轨道。
 
     参数:
         mu: float
@@ -146,6 +146,69 @@ def solve_orbit_lagrange(mu, r_0, t_target):
         # 更新半长轴
         A_new = list(state_to_kepler(r_vec_new, v_vec_new, mu))
         a_last = A_new[0]
+        
+        # 添加结果
+        r_result.append(np.concatenate((r_vec_new, v_vec_new)))
+        t_result.append(t_end)
+        A_result.append(A_new)
+    
+    return np.array(r_result), np.array(A_result), np.array(t_result)
+
+def solve_orbit_lagrange_2(mu, r_0, t_target, **kwargs):
+    """
+    拉格朗日系数法(闭合)求解轨道。
+
+    参数:
+        mu: float
+            中心天体引力常数。
+        r_0: array_like
+            初始状态向量。
+        t_target: tuple
+            求解的每一个时间节点(不是递推)
+        **kwargs: dict
+            传递给 newton_method 函数的其他参数。
+
+    返回值:
+        r: array_like
+            对应时间的状态向量数组。
+        A: array_like
+            对应于每个状态向量的轨道六根数数组（由r再次反解）。
+            [a, e, i, Omega, omega, f, E, M]
+        t: array_like
+            对应于每个状态向量的时间值数组。
+    """
+    # 初始参数（不递推，一直使用该参数）
+    A_new = list(state_to_kepler(r_0[0:3], r_0[3:], mu))
+    a_0 = A_new[0]
+    e_0 = A_new[1]
+    f_0 = A_new[5]
+    M_0 = A_new[7]
+    r_vec_0 = r_0[0:3]
+    v_vec_0 = r_0[3:]
+    r_result = []
+    t_result = []
+    A_result = []
+    
+    for t_end in t_target:
+        # 由delta_t计算delta_f
+        M_new = calculate_M(M_0, t_end, mu, a_0, e_0)
+        target_function = partial(keplers_equation_and_derivative, M=M_new, e=e_0)
+        if e_0 > 1:
+            E_new, _ = newton_method(target_function, np.log(2 * M_new / e_0), **kwargs)
+        else:
+            E_new, _ = newton_method(target_function, M_new, **kwargs)
+        f_new = E_to_f(E_new, e_0, a_0)
+        delta_f = f_new - f_0
+        
+        # 计算拉格朗日系数
+        f, g, df, dg = lagrange_coefficient_2(mu, r_vec_0, v_vec_0, delta_f)
+        
+        # 根据拉格朗日系数法计算给定时间点状态(右端是上一次循环的)
+        r_vec_new = f * r_vec_0 + g * v_vec_0
+        v_vec_new = df* r_vec_0 + dg* v_vec_0
+        
+        # 更新半长轴
+        A_new = list(state_to_kepler(r_vec_new, v_vec_new, mu))
         
         # 添加结果
         r_result.append(np.concatenate((r_vec_new, v_vec_new)))
